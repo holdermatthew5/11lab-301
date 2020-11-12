ecaa44f5be160158f91c9bf546689592921950b9
Matthew Holder <holdermatthew5@gmail.com>
2020-11-11 21:55:46 -0800

    code well commented

--- server.js
+++ server.js
@@ -1,57 +1,69 @@
 'use strict';
 
+// BRING IN DEPENDENCIES
 let express = require('express');
 let superagent = require('superagent');
 let ejs = require('ejs');
 const pg = require('pg');
+
+// PREPARE DEPENDENCIES
 const { response, urlencoded } = require('express');
 let app = express();
 require('dotenv').config();
 
+// SET DATABASE URL
 const client = new pg.Client(process.env.DATABASE_URL);
 
-
+// SET PORT
 let PORT = process.env.PORT || 3000;
+
+// SET BASE URL
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
+
+// SET VIEW ENGINE
 app.set('view engine', 'ejs');
 
-// non-dependency global variables
-// use get when not making request
-
+// ROUTES
 app.get('/', home);
 app.post('/show', show);
 app.get('/books/:id', booksDetail);
 app.post('/books', books);
 app.get('/new', search);
-// app.put('/books:id', edit);
 
+// ROUTE HANDLERS
+// HOME
 function home(request, response) {
   const sql = `SELECT * FROM books;`;
   let count = 0;
+  // RETRIEVE BOOKS FROM DATABASE
   client.query(sql).then(data => {
     count = data.rowCount;
+    // RENDER SAVED BOOKS TO PAGE
     response.render('pages/index', {'books': data.rows, 'count': count});
   }).catch((error) => {
     console.log(error);
   });
 }
 
+// MAKE A SEARCH
 function search(request, response) {
+  // RENDER SEARCH FORM
   response.render('pages/searches/new');
 }
 
+// SEARCH RESULTS
 function show(request, response) {
   let searchType = `in${request.body.choice}`;
   let searchQuery = request.body.search_query;
   let url = `https://www.googleapis.com/books/v1/volumes?q=${searchQuery}+${searchType}`;
+  // REQUEST LIST OF BOOKS FROM GOOGLE BOOKS API
   superagent.get(url).then(data => {
     let items = data.body.items;
     let img = '';
+    // USE ONLY WHAT WE NEED
     let books = items.map(obj => {
       if (obj.volumeInfo.imageLinks.smallThumbnail[4] === ':') {
-        let imgUrlArr = [];
-        img = '';
         for (let i = 0; i < obj.volumeInfo.imageLinks.smallThumbnail.length; i++) {
           imgUrlArr.push(obj.volumeInfo.imageLinks.smallThumbnail[i]);
         }
@@ -63,6 +75,7 @@ function show(request, response) {
       const book = new Book(obj, img);
       return book;
     });
+    // RENDER SEARCH RESULTS TO PAGE
     response.status(200).render('pages/searches/show', {'books': books});
   })
   .catch ((error) => {
@@ -71,37 +84,37 @@ function show(request, response) {
   });
 }
 
+// BOOK DETAILS
 function booksDetail(request, response) {
-  console.log(request.body);
-  let bookId = request.params.id;
   const select = `SELECT * FROM books WHERE id=$1;`;
   const safeVal = [bookId];
+  // RETRIEVE REQUESTED BOOK FROM DATABASE
   client.query(select, safeVal).then(data => {
     let book = data.rows;
+    // RENDER DETAILS OF BOOK TO PAGE
     response.render('pages/books/show', {'book': book});
   });
-  
 }
 
+// ADD BOOKS TO DATABASE
 function books(request, response) {
   const obj = request.body;
   const insert = 'INSERT INTO books (authors, title, isbn, image_url, description, shelf) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;';
   const safeInsert = [obj.authors, obj.title, obj.isbn, obj.image_url, obj.description, obj.shelf];
+  // ADD BOOK TO DATABASE
   client.query(insert, safeInsert)
     .then(data => {
       let newBook = data.rows[0].id;
+      // REDIRECT TO DETAILS VIEW AND PASS IN DATA NECESSARY FOR FINDING DETAILS OF SAVED BOOKS
       response.redirect(`/books/${newBook}`);
   })
-  // const select = 'SELECT * FROM books;';
-  // client.query(select).then(data => {
-  //   response.redirect('/books:id?id=');
-  // });
 }
 
 function edit(request, response) {
 
 }
 
+// TAILOR INCOMING DATA TO OUR NEEDS
 function Book(obj, image) {
   this.authors = obj.volumeInfo.authors || 'We\'re not really sure who wrote this one, sorry.',
   this.title = obj.volumeInfo.title || 'This book lost it\'s title :(',
@@ -110,5 +123,7 @@ function Book(obj, image) {
   this.description = obj.volumeInfo.description || 'We haven\'t heard about this one yet.',
   this.shelf = obj.volumeInfo.categories
 }
+
+// START OUR SERVER WITH A DATABASE CONNECTION
 client.connect();
 app.listen(PORT, () => console.log(`Now listening on port ${PORT}.`));
